cc01cebe114227793473cf82bca1905714e25166
lamhoangtung <lamhoangtung.vz@gmail.com>
2020-02-24 17:37:26 +0700

    Prep for new exp

--- train.sh
+++ train.sh
@@ -1,10 +1,10 @@
-python3 train.py --config DATA.BASEDIR=data/result_crop_augmented \
+python3 train.py --config DATA.BASEDIR=data/60k_crop_augmented_maskrcnn_coco_json \
                           MODE_FPN=True \
                           TRAIN.BASE_LR=1e-2 \
                           TRAIN.EVAL_PERIOD=1 \
                           "PREPROC.TRAIN_SHORT_EDGE_SIZE=[512, 512]" \
                           TRAIN.CHECKPOINT_PERIOD=1 \
                           DATA.NUM_WORKERS=2 \
-                          EXPERIMENT_NAME=tensorpack_maskRCNN_docrop_and_rotate_overfit_test \
+                          EXPERIMENT_NAME=MaskRCNN-R50C41x-COCO_finetune-docrop_and_rotate \
                           --load weights/COCO-MaskRCNN-R50C41x.npz \
-                          --logdir log/overfit_test
+                          --logdir log/MaskRCNN-R50C41x-COCO_finetune-docrop_and_rotate
